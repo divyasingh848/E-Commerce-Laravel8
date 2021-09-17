357205f33eef42f7a06827b17a14cf627a89ea2d
divyasingh848 <90852204+divyasingh848@users.noreply.github.com>
2021-09-17 14:05:56 +0530

    Update CartController.php

--- e_commerce/app/Http/Controllers/Frontend/CartController.php
+++ e_commerce/app/Http/Controllers/Frontend/CartController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Wishlist;
 use Auth;
 use Carbon\Carbon;
 use Gloudemans\Shoppingcart\Facades\Cart;
@@ -47,25 +46,5 @@ class CartController extends Controller
         return response()->json(['success'=>'Product remove from Cart']);
     }
 
-    public function AddToWishlist(Request $request,$product_id){
-        
-        if(Auth::check()){
-
-        $exist = Wishlist::where('user_id',Auth::id())->where('product_id',$product_id)->first();
-                 
-            if(!$exist){
-                Wishlist::insert([
-                    'user_id' => Auth::id(),
-                    'product_id' => $product_id,
-                    'created_at' => Carbon::now(),
-                ]);
-           return response()->json(['success'=>'Successfully added into wishlist']);
-            }else{
-                return response()->json(['error'=>'Product Already exist in wishlist']);
-            } 
-      
-        }else{
-            return response()->json(['error'=>'Login first']);
-        }
-    }
+   
 }
